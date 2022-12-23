2d53b67c47a4d49c4d39beb35776cf072383d78e
MdShamimIslam <shamim401897@gmail.com>
2022-12-23 19:32:45 +0600

    context finished

--- src/Pages/Homes/Banner/Banner.js
+++ src/Pages/Homes/Banner/Banner.js
@@ -1,45 +1,43 @@
-import React from 'react';
- 
- import BannerItem from './BannerItem'
+import React from "react";
+
+import BannerItem from "./BannerItem";
 const Banner = () => {
   const bannerData = [
     {
-        image: "https://raw.githubusercontent.com/rubelrana123/phographer/main/asserts/wedding3.jpg",
-        prev: 6,
-        id: 1,
-        next: 2,
-        title : "We Shoot Happiness"
+      image:
+        "https://raw.githubusercontent.com/rubelrana123/phographer/main/asserts/wedding3.jpg",
+      prev: 6,
+      id: 1,
+      next: 2,
+      title: "We Shoot Happiness",
+      details:"“Happiness” is an apocalyptic urban thriller set in the near future inside an apartment building that is stratified by social standing. The high-rise apartment is sealed off when a new infectious disease emerges, and the drama depicts the fear, psychological battles, and desperate struggles for survival that ensue."
     },
     {
-        image: "https://raw.githubusercontent.com/rubelrana123/phographer/main/asserts/weeding255520_960_720.jpg",
-        prev: 1,
-        id: 2,
-        next: 3,
-        title : `EMOTIONS OF THE DAY.`
+      image:
+        "https://raw.githubusercontent.com/rubelrana123/phographer/main/asserts/weeding255520_960_720.jpg",
+      prev: 1,
+      id: 2,
+      next: 3,
+      title: `Emotions Of The Day.`,
+      details:"We use different words to talk about sadness: agony, anguish, broken heart, hurt, sorrow, dejection, dismay, homesickness, distress, unhappiness and more. All these emotions can occur in response to a negative or unexpected situations, or life changes."
     },
     {
-        image: "https://raw.githubusercontent.com/rubelrana123/phographer/main/asserts/couple.jpg",
-        prev: 2,
-        id: 3,
-        next: 1,
-              title : `Love the life you live`
+      image:
+        "https://raw.githubusercontent.com/rubelrana123/phographer/main/asserts/couple.jpg",
+      prev: 2,
+      id: 3,
+      next: 1,
+      title: `Love The Life You Live`,
+      details:"Remember that life is a blessing; be grateful for it. And, since life is a precious gift, why do less than the best you can? Don’t forget to stay true to your own values, don’t let your own negative outlook hold you back"
     },
-  
-]
+  ];
   return (
     <div>
       <div className="carousel w-full  ">
-
-        {
-          bannerData.map(sliderInfo =>   <BannerItem key={sliderInfo.id} slide= {sliderInfo}></BannerItem> )
-        }
-      
-
-   
-   
-   
-   
-</div>
+        {bannerData.map((sliderInfo) => (
+          <BannerItem key={sliderInfo.id} slide={sliderInfo}></BannerItem>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Pages/Homes/Banner/BannerItem.js
+++ src/Pages/Homes/Banner/BannerItem.js
@@ -2,20 +2,25 @@ import React from 'react';
 
 import './Banner.css'
 const BannerItem = ({slide}) => {
-    const {image, id, prev, next, title} = slide;
+    const {image, id, prev, next, title,details} = slide;
     return (
         <div id={`slide${id}`} className="carousel-item relative w-[100%]     ">
                 <div className='carousel-img w-full'>
                     <img src={image} alt="" className="w-[100%]     " />
                 </div>
-                <div className="absolute flex justify-end transform -translate-y-1/2 left-24 top-1/2">
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-32 top-1/2">
                     <h1 className='text-6xl font-bold font-serif uppercase text-gray-100'>
                     {title}
                     </h1>
                 </div>
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-32 top-3/4 w-96">
+                    <h1 className='text-xl text-gray-100'>
+                    {details}
+                    </h1>
+                </div>
  
           
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-16 bottom-0">
                     <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a>
                     <a href={`#slide${next}`} className="btn btn-circle">❯</a>
                 </div>
